76a61b2a512c43870ed2c03061ef345bc67f7e11
Kaushik Rao <rkaushik@umich.edu>
2018-12-09 15:44:29 -0600

    Added tree / no visualization

--- ClassifierPlayground/Classifiers.py
+++ ClassifierPlayground/Classifiers.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from sklearn import tree
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -35,7 +37,7 @@ class Fischer(Classifier):
         fa = np.array([sum(ts[1] > tc) for tc in tcs])
         fb = np.array([sum(ts[2] < tc) for tc in tcs])
 
-        self.tc = tcs[np.where(np.diff(np.sign(fa - fb)) != 0)[0][0]]
+        self.tc = tcs[np.argmin(fa +fb)]
 
         return self
 
@@ -46,4 +48,24 @@ class Fischer(Classifier):
         return (self._t(np.array(x)) > self.tc) * 1 + 1
 
     def visualize(self):
-        return plt.plot(self.means[:,0], self.means[:,1], 'r->')
+        x = np.linspace(min(np.append(self.data1.x, self.data2.x)),
+                        max(np.append(self.data1.x, self.data2.x)), 100 )
+        y = (self.tc - x * self.a[0]) / self.a[1]
+        return plt.plot(x, y, label='Decision Boundary', color='pink')
+
+class Tree(Classifier):
+    def train(self, data1, data2):
+        self.data1 = pd.DataFrame(data1, columns=['x', 'y'])
+        self.data2 = pd.DataFrame(data2, columns=['x', 'y'])
+
+        self.clf = tree.DecisionTreeClassifier(max_depth=4)
+        self.clf.fit(np.concatenate((self.data1.values, self.data2.values)), 
+                     np.concatenate((np.ones(len(self.data1)), np.ones(len(self.data2.values)) * -1)))
+
+        return self
+
+    def classify(self, x):
+        return self.clf.predict(x)
+
+    def visualize(self):
+        return
