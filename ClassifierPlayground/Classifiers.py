11cf071375ea7072746eff793cea494c1f2c00ea
Kaushik Rao <rkaushik@umich.edu>
2018-12-09 22:24:12 -0600

    Added an RBF Kernal - Visualization not implemented

--- ClassifierPlayground/Classifiers.py
+++ ClassifierPlayground/Classifiers.py
@@ -144,12 +144,45 @@ class Circular(Classifier):
         plt.plot(xs, y2s, color='pink')
         return plt.plot(xs, y1s, label='Decision Boundary', color='pink')
 
+
+class RBF(Classifier):
+    def train(self, data1, data2):
+        self.data1 = pd.DataFrame(data1, columns=['x', 'y'])
+        self.data2 = pd.DataFrame(data2, columns=['x', 'y'])
+
+        within = self.data1.cov() + self.data2.cov()
+        self.means = np.array([self.data2.mean(), self.data1.mean()])
+        self.a = np.dot(np.linalg.inv(within.values), 
+                        self.data2.mean() - self.data1.mean())
+
+        ts = {1: self._t(self.data1.values),
+              2: self._t(self.data2.values)}
+
+        tcs = np.linspace(np.min(list(ts.values())), 
+                          np.max(list(ts.values())), 100)
+        fa = np.array([sum(ts[1] > tc) for tc in tcs])
+        fb = np.array([sum(ts[2] < tc) for tc in tcs])
+
+        self.tc = tcs[np.argmin(fa +fb)]
+
+        return self
+
+    def _t(self, data):
+        return np.exp(-np.linalg.norm(data - self.a, axis=1)**2)
+
+    def classify(self, x):
+        return (self._t(np.array(x)) > self.tc) * 1 + 1
+
+    def visualize(self):
+        return
+
+
 class Tree(Classifier):
     def train(self, data1, data2):
         self.data1 = pd.DataFrame(data1, columns=['x', 'y'])
         self.data2 = pd.DataFrame(data2, columns=['x', 'y'])
 
-        self.clf = tree.DecisionTreeClassifier(max_depth=3)
+        self.clf = tree.DecisionTreeClassifier(max_depth=5)
         self.clf.fit(np.concatenate((self.data1.values, self.data2.values)), 
                      np.concatenate((np.ones(len(self.data1)), np.ones(len(self.data2.values)) * -1)))
 
